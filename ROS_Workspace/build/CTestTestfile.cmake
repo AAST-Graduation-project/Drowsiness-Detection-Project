796b3dac122b3b88421a4e24c7aa664f7be499ad
MarioEzzat <marioezzat@student.aast.edu>
2023-02-22 23:27:41 +0200

    change ros workspace

---
+++ ROS_Workspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mario/Drowsiness-Detection-Project/ROS_Workspace/src
+# Build directory: /home/mario/Drowsiness-Detection-Project/ROS_Workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("drowsiness_detection_pkg")
---
+++ ROS_Workspace/build/Drowsiness_Detection_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mario/Drowsiness-Detection-Project/ROS_Workspace/src/Drowsiness_Detection_pkg
+# Build directory: /home/mario/Drowsiness-Detection-Project/ROS_Workspace/build/Drowsiness_Detection_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
