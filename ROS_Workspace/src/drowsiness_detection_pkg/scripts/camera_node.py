9e1e21e944611d6472390a87d416f92099f81a26
MarioEzzat <marioezzat@student.aast.edu>
2023-02-23 18:49:25 +0200

    EAR_code optimization

--- ROS_Workspace/src/drowsiness_detection_pkg/scripts/camera_node.py
+++ ROS_Workspace/src/drowsiness_detection_pkg/scripts/camera_node.py
@@ -12,39 +12,40 @@ from cv_bridge import CvBridge, CvBridgeError
 global bridge
 bridge = CvBridge()
 
-
+def shutdown_callback():
+    cap.release()
+    rospy.loginfo("End of camera_node")
 if __name__ == '__main__':
     
-    #rospy.init_noed(node_name) --> start the node
+    # rospy.init_noed(node_name) --> start the node
     rospy.init_node("open_camera", anonymous=True)
-    #open the camera
+    # open the camera
     cap= cv2.VideoCapture(0)
-    #rate
-    rate= rospy.Rate(10)
-    #rospy.publisher(topic_name, msg_type, queue_size)
-    pub = rospy.Publisher('my_topic', Image, queue_size=1)
-   
+    # rate
+    rate= rospy.Rate(20)
+    # rospy.publisher(topic_name, msg_type, queue_size)
+    pub = rospy.Publisher('my_topic', Image, queue_size=2)
+    # call only on shutdown
+    rospy.on_shutdown(shutdown_callback)
     while not rospy.is_shutdown():
 
         #get cv2 img
         ret,cv2_frame = cap.read()
         if ret is True:
-            
-            resized = cv2.resize(cv2_frame, (480,480), interpolation = cv2.INTER_AREA)
+            h, w, c= cv2_frame.shape
+            resized = cv2.resize(cv2_frame, (int(w*0.6),int(h*0.6)))
             #convert cv2_img to ros img
             ros_frame = bridge.cv2_to_imgmsg(resized,"bgr8")
 
             #published to the topic
             rospy.loginfo("send a frame")
             pub.publish(ros_frame)
-
-        if cv2.waitKey(1) == ord("q") or rospy.is_shutdown():
-
-            cap.release()
-            break
+            
 
         #delay
         rate.sleep()
+    
+
 
 
 
