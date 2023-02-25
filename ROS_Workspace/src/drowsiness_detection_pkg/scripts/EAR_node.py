a01e4d5ebbad1ecbd00e450149890bfdc639bd84
MarioEzzat <marioezzat@student.aast.edu>
2023-02-25 19:36:13 +0200

    Added comments for EAR_Calculation_2 file

--- ROS_Workspace/src/drowsiness_detection_pkg/scripts/EAR_node.py
+++ ROS_Workspace/src/drowsiness_detection_pkg/scripts/EAR_node.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
+###########################################################
+###########################################################
+# ROS Dependecies
 import rospy
-import cv2
-
-from Include import EAR_Calculation_2
-
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+# Import Libraries
+import cv2
+
+# Import Files
+from Include import EAR_Calculation_2
+###########################################################
+###########################################################
 
 #creat bridge
 bridge = CvBridge()
-End = False
+
+"""
+name:   my_callback
+Input:  ROS_Frame msg 
+job:    Get the state of the driver by calculating Eye Aspect Ratio
+output: cv2_Frame
+"""
 def my_callback(ros_frame):
-    #start your code 
+
+    #start your code for EAR_node 
     rospy.loginfo("received a frame")
 
-    global bridge
     #convert the frame to cv2
+    global bridge
     cv2_frame = bridge.imgmsg_to_cv2(img_msg=ros_frame, desired_encoding="bgr8")
 
     #start calculations for EAR
     cv2_frame= EAR_Calculation_2.F_Get_Face(cv2_frame)
-    rate.sleep()
+
+    
     #showing the frame
     cv2.imshow("img", cv2_frame)
+
     #delay  
-    cv2.waitKey(1) 
-    #rate.sleep()
+    cv2.waitKey(1)  # delay for showing frames 1 ms
+    rate.sleep()   # rate of receiving for the node
 
-    # end your code 
+    # end your code for EAR_node
 
+"""
+Function_name: shutdown_callback
+Input: None
+job: shutdown interrupt to close cv2_window & print end msg
+output: None
+"""
 def shutdown_callback():
     rospy.loginfo("End of EAR_node")
     cv2.destroyAllWindows()
@@ -40,8 +61,10 @@ if __name__ == '__main__':
 
     # .init_noed(node_name) --> start the node
     rospy.init_node("EAR")
+
     # set rate to publish in (x) Hz
     rate = rospy.Rate(20)
+
     # rospy.Subscriber(topic_name, msg_type, queue_size)
     rospy.Subscriber('my_topic', Image, my_callback, queue_size= 1)
     
--- ROS_Workspace/src/drowsiness_detection_pkg/scripts/Include/EAR_Calculation_2.py
+++ ROS_Workspace/src/drowsiness_detection_pkg/scripts/Include/EAR_Calculation_2.py
@@ -3,12 +3,13 @@ import mediapipe as mp
 import numpy as np
 from math import dist
 
-
+# constant values
 N_Initial_Frames = 25
 Holding_Frames = 20
 Drowsiness_Factor = 0.7
 Ignored_Initial_EAR_Values = 0.2
 
+# global static variables 
 class static:
     INITIAL_EAR = 0
     INITIAL_COUNTER=0
@@ -16,17 +17,71 @@ class static:
     SUM_EAR= 0
     STATE = "NO STATE"
 
-
+# mediapipe face mesh detector
 mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.6, min_tracking_confidence=0.7)
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.7, min_tracking_confidence=0.7,
+                                  refine_landmarks=True, max_num_faces= 1, static_image_mode=False)
 
-cap = cv2.VideoCapture(0)
+"""
+name:   F_Get_Face
+Input:  cv2_Frame 
+job:    Adding State, Cuurnt Eye Aspect Ratio & Initail Eye Apect Ratio to the cv2_Frame   
+output: cv2_frame
+"""
+def F_Get_Face(C_Frame):
+    # change encoding image from BGR to RGB
+    RGB_Frame = cv2.cvtColor(C_Frame,cv2.COLOR_BGR2RGB)
+    # improve speed
+    RGB_Frame.flags.writeable= False
+    # start detection
+    results = face_mesh.process(RGB_Frame)
+    # when the face is found
+    if results.multi_face_landmarks:
+        # get eyes coordinates in two lists from 486 landmarks 
+        left_eye,right_eye = F_LandmarksDetection(C_Frame, results)
+        # get eye aspect ratio 
+        avg_EAR = F_EAR_Calc(left_eye,right_eye)
+        # 
+        if static.INITIAL_EAR == 0:
+             F_Initial_EAR_Calc(avg_EAR)
+
+        F_Get_State(avg_EAR, static.INITIAL_EAR)
+        # put the values on the screen
+        # state
+        cv2.putText(C_Frame, ((str(static.STATE))), (10, 120),
+                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
+        # initial EAR
+        cv2.putText(C_Frame, ("init_EAR="+(str(static.INITIAL_EAR))), (10, 90),
+                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
+        # current EAR
+        cv2.putText(C_Frame, ("EAR="+(str(avg_EAR))), (10, 60),
+                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
+        
+        # for drawing the coordinates of the eyes uncomment the following lines
+
+        # cv2.circle(C_Frame, left_eye[8], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, left_eye[0], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, left_eye[4], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, left_eye[12], 2, (0,255,0),-1)
+
+        # cv2.circle(C_Frame, right_eye[8], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, right_eye[0], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, right_eye[4], 2, (0,255,0),-1)
+        # cv2.circle(C_Frame, right_eye[12], 2, (0,255,0),-1)
+    # return the frame    
+    return C_Frame    
 
 
-#left_eye = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
-#right_eye = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
-
+"""
+name:   F_LandmarksDetection
+Input:  Image & all coordinates 
+job:    save the eyes coordinates in two list (left_coord[] & right coord[])
+output: left_coord[] & right coord[]
+"""
 def F_LandmarksDetection(C_Frame, C_results):
+    #left_eye = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+    #right_eye = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
+
     img_hieght, img_wedth= C_Frame.shape[:2]
     left_coord = [(int(C_results.multi_face_landmarks[0].landmark[362].x*img_wedth), int(C_results.multi_face_landmarks[0].landmark[362].y*img_hieght)),
                   (int(C_results.multi_face_landmarks[0].landmark[382].x*img_wedth), int(C_results.multi_face_landmarks[0].landmark[382].y*img_hieght)),
@@ -73,12 +128,24 @@ def F_LandmarksDetection(C_Frame, C_results):
     
     return left_coord,  right_coord
 
+"""
+name:   F_EAR_Calc
+Input:  Left_eye[(x, y)] & Right_eye[(x, y)]
+job:    calculate Eye Aspect Ratio 
+output: the average EAR of both eyes
+"""
 def F_EAR_Calc(C_left_eye, C_right_eye):
     left_EAR = dist(C_left_eye[12],C_left_eye[4])/dist(C_left_eye[0],C_left_eye[8])
     right_EAR = dist(C_right_eye[12],C_right_eye[4])/dist(C_right_eye[0],C_right_eye[8])
     AVG_EAR = round((right_EAR+left_EAR)/2.0, 2)
     return AVG_EAR
 
+"""
+name:   F_Initial_EAR_Calc
+Input:  Current Aye Aspect Ratio
+job:    Calculates the The Intial EAR from the current EAR  
+output: None
+"""
 def F_Initial_EAR_Calc(C_avg_EAR):
     if static.INITIAL_COUNTER == N_Initial_Frames:
         static.INITIAL_EAR = round((static.SUM_EAR/N_Initial_Frames), 2)
@@ -88,49 +155,23 @@ def F_Initial_EAR_Calc(C_avg_EAR):
         static.SUM_EAR += C_avg_EAR
         static.INITIAL_COUNTER += 1
         
-    return static.INITIAL_EAR
+
+"""
+name:   F_Get_State
+Input:  Current Eye Aspect Ratio & Initial Eye Aspect Ratio
+job:    Compere The current EAR with Initial EAR  
+output: None
+"""
 def F_Get_State(C_Current_EAR, C_INITIAL_EAR):
 
-    if C_Current_EAR < (C_INITIAL_EAR*0.75):
+    if C_Current_EAR < (C_INITIAL_EAR*Drowsiness_Factor):
         static.D_COUNTER += 1
-        if static.D_COUNTER > 20:
+        if static.D_COUNTER > Holding_Frames:
             #static.flag = 1
             static.STATE = "sleep"
         
-    elif C_Current_EAR >= (C_INITIAL_EAR*0.75):
+    elif C_Current_EAR >= (C_INITIAL_EAR*Drowsiness_Factor):
         static.D_COUNTER = 0
         #static.flag = 0
         static.STATE = "awake"
     
-
-def F_Get_Face(C_Frame):
-    RGB_Frame = cv2.cvtColor(C_Frame,cv2.COLOR_BGR2RGB)
-    results = face_mesh.process(RGB_Frame)
-
-    if results.multi_face_landmarks:
-
-        left_eye,right_eye = F_LandmarksDetection(C_Frame, results)
-
-        avg_EAR = F_EAR_Calc(left_eye,right_eye)
-        if static.INITIAL_EAR == 0:
-            static.INITIAL_EAR = F_Initial_EAR_Calc(avg_EAR)
-
-        F_Get_State(avg_EAR, static.INITIAL_EAR)
-
-        cv2.putText(C_Frame, ((str(static.STATE))), (10, 120),
-                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
-        cv2.putText(C_Frame, ("init_EAR="+(str(static.INITIAL_EAR))), (10, 90),
-                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
-        cv2.putText(C_Frame, ("EAR="+(str(avg_EAR))), (10, 60),
-                cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
-        
-        # cv2.circle(Frame, left_eye[8], 2, (0,255,0),-1)
-        # cv2.circle(Frame, left_eye[0], 2, (0,255,0),-1)
-        # cv2.circle(Frame, left_eye[4], 2, (0,255,0),-1)
-        # cv2.circle(Frame, left_eye[12], 2, (0,255,0),-1)
-
-        # cv2.circle(Frame, right_eye[8], 2, (0,255,0),-1)
-        # cv2.circle(Frame, right_eye[0], 2, (0,255,0),-1)
-        # cv2.circle(Frame, right_eye[4], 2, (0,255,0),-1)
-        # cv2.circle(Frame, right_eye[12], 2, (0,255,0),-1)
-    return C_Frame    
